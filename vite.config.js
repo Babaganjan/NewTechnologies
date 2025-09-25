7c20b6651f68b360ed37531663da09998e67b105
milok <dimonrorto@gmail.com>
2025-09-25 18:38:09 +0300

    Refactoring

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import path from "path";
-import { browserslistToTargets } from "lightningcss";
-import browserslist from "browserslist";
+import { defineConfig } from 'vite';
+import path from 'path';
+import { browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
 
 export default defineConfig({
   css: {
-    transformer: "lightningcss",
+    transformer: 'lightningcss',
     lightningcss: {
-      targets: browserslistToTargets(browserslist(">= 0.25%")),
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
     },
   },
   build: {
-    cssMinify: "lightningcss",
+    cssMinify: 'lightningcss',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@img": path.resolve(__dirname, "src/assets/img"),
+      '@': path.resolve(__dirname, 'src'),
+      '@img': path.resolve(__dirname, 'src/assets/img'),
     },
   },
 });
