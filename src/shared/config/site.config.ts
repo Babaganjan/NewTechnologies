275b37e2b334b53deefb45b1dee0f9eb87c7001f
Vitaly <babaganjan170388@gmail.com>
2025-10-03 17:06:39 +0300

    Ужас

---
+++ src/shared/config/site.config.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+    title: "Новые технологии",
+    description: "",
+    navItems: [
+        {
+        href: '/',
+        title: "Сервис"
+        },
+        {
+        href: '/solution',
+        title: "IT-Решения"
+        },
+        {
+        href: '/about',
+        title: "О нас"
+        },
+        {
+        href: '/contacts',
+        title: "Контакты"
+        },
+    ]
+}
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,40 +1,52 @@
 export const Header = () => {
-  return (<header className="header">
+  return (
+    <header className="header">
       <div className="container header__container flex">
-          <div className="header__wrapper flex">
-              <nav className="header__nav nav hidden">
-                  <ul className="nav__list flex">
-                      <li className="nav__item">
-                          <a href="" className="nav__link">Сервис</a>
-                      </li>
-                      <li className="nav__item">
-                          <a href="" className="nav__link">IT-решения</a>
-                      </li>
-                      <li className="nav__item">
-                          <a href="" className="nav__link">О нас</a>
-                      </li>
-                      <li className="nav__item">
-                          <a href="" className="nav__link">Контакты</a>
-                      </li>
-                  </ul>
-              </nav>
-              <a href="" className="header__link">
-                  <img src="src/assets/img/logo.svg" alt="Логотип" className="header__logo"/>
-              </a>
+        <div className="header__wrapper flex">
+          <nav className="header__nav nav hidden">
+            <ul className="nav__list flex">
+              <li className="nav__item">
+                <a href="" className="nav__link">
+                  Сервис
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav__link">
+                  IT-решения
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav__link">
+                  О нас
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav__link">
+                  Контакты
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <a href="" className="header__link">
+            <img src="src/assets/img/logo.svg" alt="Логотип" className="header__logo" />
+          </a>
+        </div>
+        <div className="header__wrapper-contacts flex">
+          <div className="custom-select">
+            <select name="city" className="header__city">
+              <option value="almaty">Алматы</option>
+              <option value="karaganda">Караганда</option>
+              <option value="shymkent">Шымкент</option>
+            </select>
           </div>
-          <div className="header__wrapper-contacts flex">
-              <div className="custom-select">
-                  <select name="city" className="header__city">
-                      <option value="almaty">Алматы</option>
-                      <option value="karaganda">Караганда</option>
-                      <option value="shymkent">Шымкент</option>
-                  </select>
-              </div>
-              <a className="header__phone" href="tel:+74951234567">+7 (700) 346 43 71</a>
-          </div>
-          <div className="header__burger flex hidden__burger--deckstop">
-              <button className="header__btn"></button>
-          </div>
+          <a className="header__phone" href="tel:+74951234567">
+            +7 (700) 346 43 71
+          </a>
+        </div>
+        <div className="header__burger flex hidden__burger--deckstop">
+          <button className="header__btn"></button>
+        </div>
       </div>
-  </header>);
-}
+    </header>
+  );
+};
