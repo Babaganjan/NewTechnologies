3b26253d374faa7c976efdc1eecb0b2fa0df373a
milok <dimonrorto@gmail.com>
2025-10-03 19:21:52 +0300

    Done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    additionalData: `$img-path: '/img';`,
+  },
 };
 
 export default nextConfig;
