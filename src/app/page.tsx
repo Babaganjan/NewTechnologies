ea451651c6e9f2f9b3a30ce5e8b186145976b137
Vitaly <babaganjan170388@gmail.com>
2025-10-04 22:45:01 +0300

    tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { About } from '@/widgets/about/about';
-import { Advertising } from '@/widgets/advertising/advertising';
-import { FavoritesProject } from '@/widgets/favoritesProject/favoritesProject';
-import { FeedbackMenu } from '@/widgets/feedbackMenu/feedbackMenu';
-import { Hero } from '@/widgets/hero/hero';
-import { ObjectLevel } from '@/widgets/objectLevel/objectLevels';
-import { Products } from '@/widgets/products/products';
-import { ProjectMenu } from '@/widgets/projectMenu/projectMenu';
-import { Questions } from '@/widgets/questions/questions';
-import { Result } from '@/widgets/result/result';
-import { Services } from '@/widgets/services/services';
+import { About } from '@/widgets/about/about.tsx';
+import { Advertising } from '@/widgets/advertising/advertising.tsx';
+import { FavoritesProject } from '@/widgets/favoritesProject/favoritesProject.tsx';
+import { FeedbackMenu } from '@/widgets/feedbackMenu/feedbackMenu.tsx';
+import { Hero } from '@/widgets/hero/hero.tsx';
+import { ObjectLevel } from '@/widgets/objectLevel/objectLevels.tsx';
+import { Products } from '@/widgets/products/products.tsx';
+import { ProjectMenu } from '@/widgets/projectMenu/projectMenu.tsx';
+import { Questions } from '@/widgets/questions/questions.tsx';
+import { Result } from '@/widgets/result/result.tsx';
+import { Services } from '@/widgets/services/services.tsx';
 
 export default function Home() {
   return (
