5853092acef6baa792170a0cdf64da4aa4ae2004
Vitaly <babaganjan170388@gmail.com>
2025-10-04 20:12:57 +0300

    Fix Vercel output directory

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,13 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   sassOptions: {
+//     additionalData: `$img-path: '/img';`,
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,6 +15,13 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `$img-path: '/img';`,
   },
+  // Добавьте эти настройки для Vercel
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
