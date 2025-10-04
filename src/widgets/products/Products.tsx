d8d602f6f7cc4d7fb8e285f22e59620d86530664
Vitaly <babaganjan170388@gmail.com>
2025-10-04 18:20:45 +0300

    add fix

--- src/widgets/products/Products.tsx
+++ src/widgets/products/Products.tsx
@@ -1,84 +1,62 @@
 import Image from 'next/image';
+import './_products-block.scss';
 
 export const Products = () => {
   return (
-      <section className="products" aria-labelledby="products-title">
-          <div className="products__container container">
-              <div className="products__wrapper-text">
-                  <h2 id="products-title" className="products-title">Наша продукция</h2>
-                  <p className="products-subtitle">
-                      <span>надежное Оборудование,</span> соответствующее строгим
-                      <span>отраслевым</span> стандартам
-                  </p>
-              </div>
-              <ul className="products__list">
-                  <li className="products__item">
-                      <div className="products__img">
-                          <Image
-                              src="/img/turniket.png"
-                              alt="Турникеты"
-                              width={96}
-                              height={110}
-                          />
-                      </div>
-                      <div className="products__title">
-                          <h5>Турникеты</h5>
-                      </div>
-                  </li>
-                  <li className="products__item">
-                      <div className="products__img">
-                          <Image
-                              src="/img/camera.png"
-                              alt="Камеры"
-                              width={110}
-                              height={100}
-                          />
-                      </div>
-                      <div className="products__title">
-                          <h5>Видеокамеры</h5>
-                      </div>
-                  </li>
-                  <li className="products__item">
-                      <div className="products__img">
-                          <Image
-                              src="/img/commutator-v2.png"
-                              alt="Видеорегистраторы"
-                              width={120}
-                              height={54}
-                          />
-                      </div>
-                      <div className="products__title">
-                          <h5>Видеорегистраторы</h5>
-                      </div>
-                  </li>
-                  <li className="products__item">
-                      <div className="products__img">
-                          <Image
-                              src="/img/server.png"
-                              alt="Сервера"
-                              width={110}
-                              height={110}
-                          />
-                      </div>
-                      <div className="products__title">
-                          <h5>Серверные шкафы</h5>
-                      </div>
-                  </li>
-                  <li className="products__item">
-                      <div className="products__img">
-                          <Image
-                              src="/img/commutator.png"
-                              alt="Коммутаторы"
-                              width={130}
-                              height={48}
-                          />
-                      </div>
-                      <div className="products__title">
-                          <h5>Коммутаторы</h5>
-                      </div>
-                  </li>
-              </ul>
-          </div>
-      </section>
-  )
-}
+    <section className="products" aria-labelledby="products-title">
+      <div className="products__container container">
+        <div className="products__wrapper-text">
+          <h2 id="products-title" className="products-title">
+            Наша продукция
+          </h2>
+          <p className="products-subtitle">
+            <span>надежное Оборудование,</span> соответствующее строгим
+            <span>отраслевым</span> стандартам
+          </p>
+        </div>
+        <ul className="products__list">
+          <li className="products__item">
+            <div className="products__img">
+              <Image src="/img/turniket.png" alt="Турникеты" width={96} height={110} />
+            </div>
+            <div className="products__title">
+              <h5>Турникеты</h5>
+            </div>
+          </li>
+          <li className="products__item">
+            <div className="products__img">
+              <Image src="/img/camera.png" alt="Камеры" width={110} height={100} />
+            </div>
+            <div className="products__title">
+              <h5>Видеокамеры</h5>
+            </div>
+          </li>
+          <li className="products__item">
+            <div className="products__img">
+              <Image src="/img/commutator-v2.png" alt="Видеорегистраторы" width={120} height={54} />
+            </div>
+            <div className="products__title">
+              <h5>Видеорегистраторы</h5>
+            </div>
+          </li>
+          <li className="products__item">
+            <div className="products__img">
+              <Image src="/img/server.png" alt="Сервера" width={110} height={110} />
+            </div>
+            <div className="products__title">
+              <h5>Серверные шкафы</h5>
+            </div>
+          </li>
+          <li className="products__item">
+            <div className="products__img">
+              <Image src="/img/commutator.png" alt="Коммутаторы" width={130} height={48} />
+            </div>
+            <div className="products__title">
+              <h5>Коммутаторы</h5>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
--- src/widgets/questions/Questions.tsx
+++ src/widgets/questions/Questions.tsx
@@ -1,54 +1,60 @@
+import './_questions-block.scss';
+
 export const Questions = () => {
   return (
-      <section className="questions" aria-labelledby="questions-title">
-          <div className="questions__container container">
-              <h2 id="questions-title" className="questions__title">Частые вопросы</h2>
-              <ul className="card-wrapper">
-                  <li className="questions__card card--a">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">С какими объектами вы работаете?</h5>
-                  </li>
-                  <li className="questions__card card--b">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">Можно ли интегрировать системы между собой?</h5>
-                  </li>
-                  <li className="questions__card card--c">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">
-                          Работает ли система сигнализации при отключении электричества?
-                      </h5>
-                  </li>
-                  <li className="questions__card card--d">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">Как происходит проектирование и установка?</h5>
-                  </li>
-                  <li className="questions__card card--e">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">Предоставляете ли вы техническое обслуживание?</h5>
-                  </li>
-                  <li className="questions__card card--f">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">
-                          У вас есть оборудование в наличии или нужно ждать поставку?
-                      </h5>
-                  </li>
-                  <li className="questions__card card--g">
-                      <div className="questions__card-decorative"></div>
-                      <h5 className="questions__content">Работаете ли вы с государственными организациями?</h5>
-                  </li>
-                  <li className="questions__card card--h">
-                      <div className="questions__card-decorative"></div>
-                      <p className="questions__content">Сколько времени занимает установка систем?</p>
-                  </li>
-                  <li className="questions__card--active questions__card">
-                      <a href="#">
-                          <h4 className="questions__card-title">Все еще есть вопросы?</h4>
-                          <span className="questions__card-subtitle">Мы готовы помочь.</span>
-                          <div className="questions__card-arrow--active"></div>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-      </section>
-  )
-}
+    <section className="questions" aria-labelledby="questions-title">
+      <div className="questions__container container">
+        <h2 id="questions-title" className="questions__title">
+          Частые вопросы
+        </h2>
+        <ul className="card-wrapper">
+          <li className="questions__card card--a">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">С какими объектами вы работаете?</h5>
+          </li>
+          <li className="questions__card card--b">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">Можно ли интегрировать системы между собой?</h5>
+          </li>
+          <li className="questions__card card--c">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">
+              Работает ли система сигнализации при отключении электричества?
+            </h5>
+          </li>
+          <li className="questions__card card--d">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">Как происходит проектирование и установка?</h5>
+          </li>
+          <li className="questions__card card--e">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">Предоставляете ли вы техническое обслуживание?</h5>
+          </li>
+          <li className="questions__card card--f">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">
+              У вас есть оборудование в наличии или нужно ждать поставку?
+            </h5>
+          </li>
+          <li className="questions__card card--g">
+            <div className="questions__card-decorative"></div>
+            <h5 className="questions__content">
+              Работаете ли вы с государственными организациями?
+            </h5>
+          </li>
+          <li className="questions__card card--h">
+            <div className="questions__card-decorative"></div>
+            <p className="questions__content">Сколько времени занимает установка систем?</p>
+          </li>
+          <li className="questions__card--active questions__card">
+            <a href="#">
+              <h4 className="questions__card-title">Все еще есть вопросы?</h4>
+              <span className="questions__card-subtitle">Мы готовы помочь.</span>
+              <div className="questions__card-arrow--active"></div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
