3752af14d7d141725b974d3b5d700e87be6ba87a
milok <dimonrorto@gmail.com>
2025-10-09 16:14:00 +0300

    Remove deprecated build artifacts and unused files from the dist directory to streamline the project structure and reduce build size.

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bKjgdalzEpetP+DRjWYwHponhKfpxq71qGDHS0z7VA0=\"\n}"
--- dist/static/chunks/src_app_page_tsx_bca56d83._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_widgets_0d1de9bf._.css"
-],
-    source: "dynamic"
-});
