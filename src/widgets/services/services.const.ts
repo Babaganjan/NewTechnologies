928fcdaf117e894732b57005d04d2a4ea2ab0ff4
milok <dimonrorto@gmail.com>
2025-10-11 16:00:56 +0300

    Refactor: Replace hardcoded service and object level items with dynamic data mapping for improved maintainability and scalability

---
+++ src/widgets/services/services.const.ts
@@ -0,0 +1,19 @@
+export const SERVICES_ITEMS = [
+  {
+    id: 1,
+    title: 'Системы защиты',
+  },
+  {
+    id: 2,
+    title: 'IT-решения',
+  },
+
+  {
+    id: 3,
+    title: 'Galion автоматизация',
+  },
+  {
+    id: 4,
+    title: 'Iiko автоматизация',
+  },
+];
