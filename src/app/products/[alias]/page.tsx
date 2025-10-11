9cd5a8a77772a4773681b86be6537deb36d34893
milok <dimonrorto@gmail.com>
2025-10-11 16:03:00 +0300

    Enhance metadata generation for product pages by dynamically setting title and description based on product alias for improved SEO and user experience.

--- src/app/products/[alias]/page.tsx
+++ src/app/products/[alias]/page.tsx
@@ -1,4 +1,12 @@
 import { PRODUCTS_DATA } from '@/widgets/products/products.const';
+export async function generateMetadata({ params }: { params: { alias: string } }) {
+  const product = PRODUCTS_DATA.find((product) => product.alias === params.alias);
+
+  return {
+    title: product?.title,
+    description: product?.title,
+  };
+}
 
 export async function generateStaticParams() {
   return PRODUCTS_DATA.map((product) => ({
