f2a11ac4832bc36567e76af7f50ac42089c2931b
milok <dimonrorto@gmail.com>
2025-10-16 21:38:48 +0300

    Refactor Hero widget: Replace Heading component with H for improved semantic structure and consistency across headings.

--- src/widgets/hero/Hero.tsx
+++ src/widgets/hero/Hero.tsx
@@ -1,28 +1,27 @@
-import { Heading } from '@/shared/ui/Heading';
+import { H } from '@/shared/ui';
 import { HomeHeading } from '@/shared/ui/HeadingSvg/HomeHeading';
-
 import './_hero.scss';
 
 export const Hero = () => {
   return (
     <section className="hero with-bottom-trapezoid">
       <div className="container hero__container flex">
-        <Heading level={1} className="heading">
+        <H level={'1'} className="heading">
           <HomeHeading />
-        </Heading>
+        </H>
         <div className="hero__content flex">
-          <Heading level={2} className="hero__title title">
+          <H level={'2'} className="hero__title title">
             Мы&nbsp;предлагаем
-          </Heading>
+          </H>
           <p className="hero__prev">
             инновационные решения для обеспечения безопасности: системы видеонаблюдения,
             охранно-пожарная сигнализация, пожаротушение, контроль доступа и&nbsp;системы
             оповещения.
           </p>
         </div>
-        <Heading level={3} className="hero__subtitle subtitle">
+        <H level={'3'} className="hero__subtitle subtitle">
           Безопасность&nbsp;нового поколения
-        </Heading>
+        </H>
       </div>
     </section>
   );
