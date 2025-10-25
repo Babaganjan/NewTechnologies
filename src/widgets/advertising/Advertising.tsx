9e06b084bcd2edf2e75c0b5217411dae3ae9e62f
milok <dimonrorto@gmail.com>
2025-10-25 14:30:03 +0300

    Refactor advertising block styles and update component structure: Rename subtitle container, enhance subtitle styling for desktop and mobile views, and adjust imports in Advertising component.

--- src/widgets/advertising/Advertising.tsx
+++ src/widgets/advertising/Advertising.tsx
@@ -1,5 +1,5 @@
-import './_advertising-block.scss';
 import { H } from '@/shared/ui';
+import './_advertising-block.scss';
 
 import { PARTNERS } from './advertising.const';
 
@@ -10,12 +10,17 @@ export const Advertising = () => {
         <H level={'2'} id="advertising-title" className="advertising__title title">
           Наши партнеры
         </H>
-        <H level={'3'} className="advertising__subtitle subtitle">
-          Работаем с лидерами
-          <span>
-            рынка из <span className="advertising__subtitle--break">отрасли</span>
-          </span>
-        </H>
+        <div className="advertising__subtitle-wrapper">
+          <H level={'3'} className="advertising__subtitle">
+            <span className="advertising__subtitle-line1">
+              Работаем с лидерами <span className="advertising__subtitle-desktop-word">рынка</span>
+            </span>
+            <span className="advertising__subtitle-line2">
+              <span className="advertising__subtitle-mobile-word">рынка </span>
+              из <span className="advertising__subtitle--break">отрасли</span>
+            </span>
+          </H>
+        </div>
         <div className="continuous-scroll-container">
           <div className="continuous-scroll-wrapper">
             {PARTNERS.map((slide) => {
