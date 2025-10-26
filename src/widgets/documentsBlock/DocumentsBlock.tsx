eb3aad6f489cdbb92f0bebaadab4072f93d8f464
milok <dimonrorto@gmail.com>
2025-10-26 18:00:42 +0300

    Remove unused state declaration in DocumentsBlock component to clean up code.

--- src/widgets/documentsBlock/DocumentsBlock.tsx
+++ src/widgets/documentsBlock/DocumentsBlock.tsx
@@ -11,7 +11,6 @@ import './Documents.scss';
 
 export const DocumentsBlock = () => {
   const [isActive, setIsActive] = useState('one');
-  const [];
 
   return (
     <section className="documents-section">
