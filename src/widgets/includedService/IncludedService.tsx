ef2acfb1d011c5e5993b00e22ac0786d5500003f
milok <dimonrorto@gmail.com>
2025-11-01 20:09:51 +0300

    Refactor IncludedService component: enhance SCSS structure with nested styles, add logo and wrapper elements, and improve JSX layout for better organization and responsiveness.

--- src/widgets/includedService/IncludedService.tsx
+++ src/widgets/includedService/IncludedService.tsx
@@ -12,15 +12,22 @@ export const IncludedService = ({ type }: { type: IncludesType }) => {
   return (
     <section className="includedService">
       <div className="includedService__container container">
-        <div className="includedService__title">
-          <H level="2" variant="dark">
-            Что вы получаете
-          </H>
-        </div>
-        <div className="includedService__subtitle">
-          <H level="3">6 причин довериться нашей системе безопасности</H>
+        <div className="includedService__titleAndSubtitle-container">
+          <div className="includedService__title">
+            <H level="2" variant="dark">
+              Что вы получаете
+            </H>
+          </div>
+          <div className="includedService__subtitle">
+            <H level="3">6 причин довериться нашей системе безопасности</H>
+          </div>
         </div>
         <div className="includedService__wrapper">
+          <div className="includedService__logo-wrapper">
+            <div className="includedService__logo">
+              <span>NT</span>
+            </div>
+          </div>
           <div className="includedService__img">
             <div className="includedService__img-decoration" aria-hidden="true"></div>
             <Image
@@ -31,8 +38,8 @@ export const IncludedService = ({ type }: { type: IncludesType }) => {
             />
           </div>
           <ul className="includedService__list">
-            {data.map((item) => (
-              <li className="includedService__item" key={item.title}>
+            {data.map((item, index) => (
+              <li className="includedService__item" key={item.title} data-index={index + 1}>
                 {item.title}
               </li>
             ))}
