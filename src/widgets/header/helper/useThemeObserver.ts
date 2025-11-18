cbf468757c392046b56884b4197548a2eba36395
milok <dimonrorto@gmail.com>
2025-11-18 23:01:17 +0300

    Refactor useThemeObserver hook to improve theme management; add pathname dependency for responsive theme updates and optimize section observation with a timeout for better performance.

--- src/widgets/header/helper/useThemeObserver.ts
+++ src/widgets/header/helper/useThemeObserver.ts
@@ -1,45 +1,42 @@
 'use client';
+import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export const useHeaderThemeObserver = (defaultTheme: 'dark' | 'light' = 'dark') => {
   const [observedTheme, setObservedTheme] = useState<'dark' | 'light'>(defaultTheme);
+  const pathname = usePathname(); // Отслеживаем изменения маршрута
 
   useEffect(() => {
+    setObservedTheme(defaultTheme);
     const handleIntersect = (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          // Читаем атрибут (может быть null)
           const attributeValue = entry.target.getAttribute('data-header-theme');
-
-          // Устанавливаем тему: если атрибут есть, берем его значение;
-          // если нет — используем 'light' (белый фон).
           const newTheme = (attributeValue || 'light') as 'dark' | 'light';
 
           setObservedTheme(newTheme);
-
-          // (В дебаг-режиме)
           console.log('New computed theme:', newTheme);
         }
       });
     };
 
-    // Триггер сработает, когда секция коснется линии, отступающей на 80px от верха экрана
-    // (примерная высота хедера)
     const observer = new IntersectionObserver(handleIntersect, {
       root: null,
       rootMargin: '-60px 0px -90% 0px',
       threshold: 0,
     });
 
-    // Ищем все секции, у которых есть атрибут data-header-theme
-    const sections = document.querySelectorAll('section');
+    const timeoutId = setTimeout(() => {
+      const sections = document.querySelectorAll('section');
 
-    sections.forEach((section) => observer.observe(section));
+      sections.forEach((section) => observer.observe(section));
+    }, 0);
 
     return () => {
+      clearTimeout(timeoutId);
       observer.disconnect();
     };
-  }, []);
+  }, [pathname, defaultTheme]);
 
   return observedTheme;
 };
