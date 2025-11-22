eb1edd7498945aaf6eb4fd9ae6f3a0fe5dd24b5e
milok <dimonrorto@gmail.com>
2025-11-22 20:55:23 +0300

    Refactor ProductsPages and related components to enhance gallery display, improve layout styles, and update key specifications. Adjusted translations in Breadcrumbs constants for consistency and clarity.

--- src/widgets/ProductsPages/gallerySpec/index.tsx
+++ src/widgets/ProductsPages/gallerySpec/index.tsx
@@ -1,35 +1,41 @@
 import Image from 'next/image';
 
 import './gallerySpec.scss';
+
+import { Breadcrumbs } from '@/shared/ui';
+
+import { myImages } from './gallery.const';
 import { getModifierClass } from './helpers/getModifierClass';
 import { getSizes } from './helpers/getSizes';
 
-interface GallerySpecProps {
-  images?: string[];
-}
-
-export const GallerySpec = ({ images = [] }: GallerySpecProps) => {
-  const count = images.length;
+export const GallerySpec = () => {
+  const count = myImages.length;
 
   const modifierClass = getModifierClass(count);
 
   return (
     <section className="gallery-section">
-      <div className={`gallery ${modifierClass} container`}>
-        {images.map((src, index) => (
-          <div key={`gallery-${index}`} className="gallery__item">
-            <Image
-              src={src}
-              alt={`Изображение продукта ${index + 1}`}
-              fill
-              className="gallery__img"
-              sizes={getSizes(count, index)}
-              loading={index === 0 ? 'eager' : 'lazy'}
-              priority={index === 0}
-              quality={85}
-            />
-          </div>
-        ))}
+      <Breadcrumbs />
+      <div className="gallery__container container">
+        <h1>
+          Сетевая цилиндрическая ИК-камера <span>NT-ipr5122-m</span>
+        </h1>
+        <div className={`gallery ${modifierClass} `}>
+          {myImages.map((src, index) => (
+            <div key={`gallery-${index}`} className="gallery__item">
+              <Image
+                src={src}
+                alt={`Изображение продукта ${index + 1}`}
+                fill
+                className="gallery__img"
+                sizes={getSizes(count, index)}
+                loading={index === 0 ? 'eager' : 'lazy'}
+                priority={index === 0}
+                quality={85}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/widgets/ProductsPages/keySpec/index.tsx
+++ src/widgets/ProductsPages/keySpec/index.tsx
@@ -1,21 +1,50 @@
+import clsx from 'clsx';
+
+import { H } from '@/shared/ui';
+
+import { specKeyData } from './keySpec.const';
 import './keySpec.scss';
-import type { SpecItem } from './keySpec.types';
+import type { KeySpecsProps } from './keySpec.types';
 
-interface KeySpecsProps {
-  title?: string;
-  items: SpecItem[];
-}
+export const KeySpecs = ({ title = 'Основные характеристики', type }: KeySpecsProps) => {
+  const data = specKeyData[type];
 
-export const KeySpecs = ({ title = 'Основные характеристики', items = [] }: KeySpecsProps) => {
   return (
-    <section>
+    <section className="keySpecs">
       <div className="container">
-        <h2>{title}</h2>
-        <div>
-          {items.map((item) => (
-            <div key={item.id}>gfeg</div>
-          ))}
+        <div className="keySpecs__title">
+          <H level="2" variant="dark">
+            {title}
+          </H>
         </div>
+        <ul className={`keySpecs__list keySpecs__list--${data.layout}`}>
+          {data.item.map((item, index) => (
+            <li
+              key={item.id}
+              className={clsx(
+                `keySpecs__item keySpecs__item--${index + 1}`,
+                item.subtitle && 'keySpecs__item--with-subtitle',
+                item.type === 'big' && 'keySpecs__item--with-value-layout',
+                item.type === 'subtitle' && 'keySpecs__item--subtitle',
+                item.type === 'singleValue' && 'keySpecs__item--singleValue',
+                item.type === 'titleEmpty' && 'keySpecs__item--titleEmpty',
+                item.type === 'titleNone' && 'keySpecs__item--titleNone'
+              )}
+            >
+              {item.title && <h3 className="keySpecs__item-title">{item.title}</h3>}
+
+              {item.value && (
+                <div className="keySpecs__item-wrapper">
+                  <p className="keySpecs__item-value">{item.value}</p>
+                  <span className="keySpecs__item-unit">{item.unit}</span>
+                </div>
+              )}
+
+              {item.description && <p className="keySpecs__item-description">{item.description}</p>}
+              {item.subtitle && <p className="keySpecs__item-description">{item.subtitle}</p>}
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
