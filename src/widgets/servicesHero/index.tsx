66eb7382a0ae0b912664155a6c1a2136d11bf1f7
Vitaly <babaganjan170388@gmail.com>
2025-11-25 21:03:50 +0300

    fv

--- src/widgets/servicesHero/index.tsx
+++ src/widgets/servicesHero/index.tsx
@@ -7,8 +7,38 @@ export const ServicesHero = () => {
     <section className="services-hero" data-header-theme="dark">
       <div className="services-hero__container container">
         <div className="services-hero__video-in-container">
-          <video autoPlay muted loop playsInline preload="metadata" aria-hidden="true">
-            <source src="/video/service-video-1920.mp4" type="video/mp4" />
+          <video
+            className="services-hero__video services-hero__video--mobile"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="metadata"
+            aria-hidden="true"
+          >
+            <source src="/video/camera-scan-360.mp4" type="video/mp4" />
+          </video>
+          <video
+            className="services-hero__video services-hero__video--tablet"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="metadata"
+            aria-hidden="true"
+          >
+            <source src="/video/camera-scan-768.mp4" type="video/mp4" />
+          </video>
+          <video
+            className="services-hero__video services-hero__video--desktop"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="metadata"
+            aria-hidden="true"
+          >
+            <source src="/video/camera-scan-1920.mp4" type="video/mp4" />
           </video>
         </div>
       </div>
