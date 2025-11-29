96b2f5c99ab21e22c0e2a656759f7647557dd776
milok <dimonrorto@gmail.com>
2025-11-29 17:42:51 +0300

    Refactor FeedbackMenu component: restructure SCSS for improved layout, enhance background image handling, and update JSX structure for better semantic organization.

--- src/widgets/feedbackMenu/index.tsx
+++ src/widgets/feedbackMenu/index.tsx
@@ -1,4 +1,3 @@
-// components/FeedbackMenu/FeedbackMenu.tsx
 'use client';
 import useModal from '@/hooks/useModal';
 import { Button, H } from '@/shared/ui';
@@ -13,20 +12,22 @@ export const FeedbackMenu = () => {
   return (
     <>
       <section className="feedback-menu" aria-labelledby="feedback-menu-title">
-        <div className="feedback-menu__wrapper container">
-          <H level={'3'} variant="dark" id="feedback-menu-title" className="feedback-menu__title">
-            Рассчитать решение под ваш объект?
-          </H>
-          <Button
-            type="button"
-            variant="feedback"
-            icon
-            onClick={handleOpenConsultation}
-            aria-haspopup="dialog"
-            aria-expanded={isConsultationModalOpen}
-          >
-            Подобрать решение
-          </Button>
+        <div className="container">
+          <div className="feedback-menu__wrapper">
+            <H level={'3'} variant="dark" id="feedback-menu-title" className="feedback-menu__title">
+              Рассчитать решение под ваш объект?
+            </H>
+            <Button
+              type="button"
+              variant="feedback"
+              icon
+              onClick={handleOpenConsultation}
+              aria-haspopup="dialog"
+              aria-expanded={isConsultationModalOpen}
+            >
+              Подобрать решение
+            </Button>
+          </div>
         </div>
       </section>
 
