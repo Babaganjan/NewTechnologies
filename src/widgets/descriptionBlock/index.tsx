27b172e98ba2a39248ea31328dcbc13a451f2119
Vitaly <babaganjan170388@gmail.com>
2025-11-30 20:18:29 +0300

    add pages forma

--- src/widgets/descriptionBlock/index.tsx
+++ src/widgets/descriptionBlock/index.tsx
@@ -1,4 +1,8 @@
+'use client';
+import useModal from '@/hooks/useModal';
 import { Button, H } from '@/shared/ui';
+
+import { FormaConsultation } from '../forma';
 
 import type { DescriptionTypeKey } from './description.const';
 import { DESCRIPTION__DATA } from './description.const';
@@ -6,37 +10,46 @@ import { DESCRIPTION__DATA } from './description.const';
 import './description.scss';
 
 export const Description = ({ type }: { type: DescriptionTypeKey }) => {
+  const { isConsultationModalOpen, handleOpenConsultation, handleCloseConsultation } = useModal({
+    initialValue: false,
+  });
   const data = DESCRIPTION__DATA[type];
 
   return (
-    <section className="description">
-      <div className="description__container container">
-        <div className="description__title-wrapper">
-          <div className="description__title">
-            <H level="2">Описание</H>
-          </div>
-          {/* <H level="4" className="description__subtitle">
-            {data.highlightTitle && <span>{data.highlightTitle}</span>}
-            {data.title}
-          </H> */}
+    <>
+      <section className="description">
+        <div className="description__container container">
+          <div className="description__title-wrapper">
+            <div className="description__title">
+              <H level="2">Описание</H>
+            </div>
 
-          <h3 className="description__subtitle">
-            {data.highlightTitle && <span>{data.highlightTitle}</span>}
-            {data.title}
-          </h3>
-        </div>
-        <div className="description__wrapper">
-          <div className="description__title-description">
-            <p className="description__title-one">{data.subtitleOne}</p>
-            <p className="description__title-two">{data.subtitleTwo}</p>
+            <h3 className="description__subtitle">
+              {data.highlightTitle && <span>{data.highlightTitle}</span>}
+              {data.title}
+            </h3>
           </div>
-          <div className="description-button-container">
-            <Button variant="description" icon className="description-button">
-              Заказать услугу
-            </Button>
+          <div className="description__wrapper">
+            <div className="description__title-description">
+              <p className="description__title-one">{data.subtitleOne}</p>
+              <p className="description__title-two">{data.subtitleTwo}</p>
+            </div>
+            <div className="description-button-container">
+              <Button
+                variant="description"
+                icon
+                className="description-button"
+                onClick={handleOpenConsultation}
+              >
+                Заказать услугу
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {isConsultationModalOpen && (
+        <FormaConsultation onSubmit={handleCloseConsultation} onClose={handleCloseConsultation} />
+      )}
+    </>
   );
 };
